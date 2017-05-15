a566685c5d2740a01fa10c94caf696d1532e05a3
cendi2005 <920941399@qq.com>
2017-05-15 23:32:37 +0800

    test

---
+++ src/package1/LockSupportIntDemo2.java
@@ -0,0 +1,37 @@
+package package1;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * Created by admin on 2017/5/15.
+ */
+public class LockSupportIntDemo2 {
+    public static Object u = new Object();
+    static ChangeObjectThread t1 = new ChangeObjectThread("t1");
+    static ChangeObjectThread t2 = new ChangeObjectThread("t2");
+
+    public static class ChangeObjectThread extends Thread {
+        public ChangeObjectThread(String name) {
+            super.setName(name);
+        }
+
+        public void run() {
+            synchronized (u) {
+                System.out.println("in " + getName());
+                LockSupport.park();
+                if (Thread.interrupted()) {
+                    System.out.println(getName() + " bei zhong duan le!");
+                }
+            }
+            System.out.println(getName() + " zhi xing jie shu");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        t1.start();
+        Thread.sleep(100);
+        t2.start();
+        t1.interrupt();
+        LockSupport.unpark(t2);
+    }
+}
