cf4cd83d0e5ea8474cee82f74f3364157369d812
cendi2005 <920941399@qq.com>
2017-10-08 14:11:20 +0800

    test

---
+++ src/join/Join.java
@@ -0,0 +1,27 @@
+package join;
+
+public class Join {
+    public static void main(String[] args) throws Exception {
+        Thread previous = Thread.currentThread();
+        for (int i = 0; i < 3; i++) {
+// 每个线程拥有前一个线程的引用，需要等待前一个线程终止，才能从等待中返回
+            Thread thread = new Thread(new Domino(previous), String.valueOf(i));
+            thread.start();
+            previous = thread;
+        }
+        System.out.println(Thread.currentThread().getName() + " terminate. main");
+    }
+    static class Domino implements Runnable {
+        private Thread thread;
+        public Domino(Thread thread) {
+            this.thread = thread;
+        }
+        public void run() {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+            }
+            System.out.println("subthread.."+Thread.currentThread().getName() + " terminate.");
+        }
+    }
+}
