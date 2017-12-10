749223c27f33427ccd273b515f109a35c23b15ca
cendi2005 <920941399@qq.com>
2017-12-10 18:10:44 +0800

    test

--- src/downloadfile/FileThread1.java
+++ src/downloadfile/FileThread1.java
@@ -1,18 +1,16 @@
 package downloadfile;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.Callable;
 
-public class FileThread extends Thread{
+public class FileThread1 implements Callable<Integer> {
     private String filePath;
     private int seekSize;
     private int canReadSize;
 
-    public FileThread(String filePath,int seekSize,int canReadSize){
+    public FileThread1(String filePath, int seekSize, int canReadSize){
         this.filePath = filePath;
         this.seekSize = seekSize;
         this.canReadSize = canReadSize;
@@ -21,14 +19,13 @@ public class FileThread extends Thread{
     int hasRead = 0;
     int totalHasRead = 0;
 
-    @Override
     public void run() {
         //file
         try {
 
             //file position
             RandomAccessFile file = new RandomAccessFile(filePath,"rw");
-            RandomAccessFile store = new RandomAccessFile("/Users/admin/Desktop/thread/src/downloadfile/2.png","rw");
+            RandomAccessFile store = new RandomAccessFile("/Users/admin/Desktop/thread/src/downloadfile/2.txt","rw");
 
             //seek the unread byte
             file.seek((long)seekSize);
@@ -36,11 +33,9 @@ public class FileThread extends Thread{
 
             System.out.println(Thread.currentThread().getName()+" start at position "+seekSize);
             System.out.println(Thread.currentThread().getName()+" can read canReadSize is  "+canReadSize);
-
             //byte cache
-            byte[] buffer = new byte[1024*1024];
-
-
+//            byte[] buffer = new byte[1024*1024];
+            byte[] buffer = new byte[3];
 
             long start = System.currentTimeMillis();
             while(totalHasRead<canReadSize&&((hasRead=file.read(buffer))!=-1)) {
@@ -65,5 +60,41 @@ public class FileThread extends Thread{
             e.printStackTrace();
         }
 
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        int length = 0;
+
+        //file position
+        RandomAccessFile file = new RandomAccessFile(filePath,"rw");
+        RandomAccessFile store = new RandomAccessFile("/Users/admin/Desktop/thread/src/downloadfile/2.png","rw");
+
+        //seek the unread byte
+        file.seek((long)seekSize);
+        store.seek((long)seekSize);
+
+        System.out.println(Thread.currentThread().getName()+" start at position "+seekSize);
+        System.out.println(Thread.currentThread().getName()+" can read canReadSize is  "+canReadSize);
+
+        //byte cache
+        byte[] buffer = new byte[3];
+
+
+
+        long start = System.currentTimeMillis();
+        while(totalHasRead<canReadSize&&((hasRead=file.read(buffer))!=-1)) {
+            System.out.println();
+            store.write(buffer,0,hasRead);
+            totalHasRead += hasRead;
+            length += hasRead;
+            System.out.println(Thread.currentThread().getName() + " totalHasRead " + totalHasRead);
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println("therad " + Thread.currentThread().getName() +" use time is "+(end-start));
+        //close stream
+        store.close();
+        return length;
     }
 }
--- src/downloadfile/Util.java
+++ src/downloadfile/Util.java
@@ -1,6 +1,12 @@
 package downloadfile;
 
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
 
 public class Util {
 
@@ -8,7 +14,7 @@ public class Util {
     public static void main(String[] args) throws Exception{
 
 
-        RandomAccessFile read = new RandomAccessFile("/Users/admin/Desktop/thread/src/downloadfile/1.zip","rw");
+        RandomAccessFile read = new RandomAccessFile("/Users/admin/Desktop/thread/src/downloadfile/1.png","rw");
 
         int filesize = (int)read.length();
 
@@ -16,7 +22,7 @@ public class Util {
 
 
 
-        int threadCount = 10;
+        int threadCount = 3;
         int readSize = 0;
         if(filesize%threadCount==0){
             readSize = filesize/threadCount;
@@ -26,11 +32,42 @@ public class Util {
 
         System.out.println("readSize:"+readSize);
 
+//
+//        for(int i=0;i<threadCount;i++){
+//            FileThread fileThread = new FileThread("/Users/admin/Desktop/thread/src/downloadfile/1.png",i*readSize,readSize);
+//            fileThread.start();
+//        }
 
-        for(int i=0;i<threadCount;i++){
-            FileThread fileThread = new FileThread("/Users/admin/Desktop/thread/src/downloadfile/1.pdf",i*readSize,readSize);
-            fileThread.start();
+        List<FutureTask<Integer>> taskList = new ArrayList<FutureTask<Integer>>();
+
+        // 创建线程池
+        ExecutorService exec = Executors.newFixedThreadPool(threadCount);
+        for (int i = 0; i < 2; i++) {
+            // 传入Callable对象创建FutureTask对象
+            FutureTask<Integer> ft = new FutureTask<Integer>(new FileThread1("/Users/admin/Desktop/thread/src/downloadfile/1.png",i*readSize,readSize));
+            taskList.add(ft);
+            // 提交给线程池执行任务，也可以通过exec.invokeAll(taskList)一次性提交所有任务;
+            exec.submit(ft);
         }
+
+        System.out.println("所有计算任务提交完毕, 主线程接着干其他事情！");
+
+        // 开始统计各计算线程计算结果
+        Integer totalResult = 0;
+        for (FutureTask<Integer> ft : taskList) {
+            try {
+                //FutureTask的get方法会自动阻塞,直到获取计算结果为止
+                totalResult = totalResult + ft.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // 关闭线程池
+        exec.shutdown();
+        System.out.println("多任务计算后的总结果是:" + totalResult);
 
 
     }
